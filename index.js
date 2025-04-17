e84528ca4801ebf7f2bc46e9e380b99df2f0dc79
RunyararoMucheche <your_email@example.com>
2025-04-17 17:35:23 -0400

    Add function usage within html code

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+function getMood() {
+  const mood = ["Happy", "Sad", "Angry", "Quiet", "Mellow"];
+  return mood[Math.floor(Math.random() * mood.length)];
+}
+
 class JSXDemo extends React.Component {
   render() {
     return (
       <div>
-        <h1>My Image!</h1>
-        <img src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <h1>My Mood is: {getMood()}</h1>
       </div>
     );
   }
